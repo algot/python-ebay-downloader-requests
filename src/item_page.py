b0e87b6c7e5d1c848b25ae22761392e0262ea20d
algot <algot@ua.fm>
2022-11-14 22:34:51 +0200

    item page get images from carousel

--- src/item_page.py
+++ src/item_page.py
@@ -1,2 +1,21 @@
+import re
+
+import requests
+
+
 class ItemPage:
-    pass
+    def __init__(self, item_url):
+        self.item_url = item_url
+        self.page_content = ''
+
+    def get_image_urls(self):
+        self._get_page_content()
+
+        regex = re.compile('https://i\.ebayimg\.com/images/g/\S{16}/s-l1600.jpg')
+        images_urls_all = regex.findall(self.page_content)
+        print(f'Number of images found on item page: {len(images_urls_all)}')
+        return images_urls_all
+
+    def _get_page_content(self):
+        with requests.get(url=self.item_url) as r:
+            self.page_content = r.text
--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import sys
 
 from index_page import IndexPage
+from item_page import ItemPage
 
 
 def main(input_filename):
-    index_page = IndexPage(filename)
+    index_page = IndexPage(input_filename)
     items_urls = index_page.get_list_of_item_urls_from_index_page()
+
+    for url in items_urls:
+        item_page = ItemPage(url)
+        item_page.get_image_urls()
     pass
 
 
 if __name__ == '__main__':
     # filename = (sys.argv[1])
 
-    filename = '2022.09.24-5452graham-003.html'  # 3
+    # filename = '2022.09.24-5452graham-003.html'  # 3
     # filename = '2022.11.05-aherl-1-240.html'  # 240
-    # filename = '2022.11.05-xroads13-01-240.html'  # 60
+    filename = '2022.11.05-xroads13-01-240.html'  # 60
     # filename = '2022.11.12-carbooks0947-008.html'  # 8
 
     main(filename)
