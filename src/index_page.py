24deed812f4dcb9afc550643065f5fbeb4eb9030
algot <algot@ua.fm>
2022-11-14 21:02:29 +0200

    first working index page

---
+++ src/index_page.py
@@ -0,0 +1,27 @@
+import re
+
+from pathlib import Path
+
+
+class IndexPage:
+    def __init__(self, filename):
+        self.parent_dir = Path(__file__).parent.parent
+        self.resource_dir = self.parent_dir.joinpath('resources')
+        self.index_page_file_path = f'{self.resource_dir.joinpath(filename)}'
+        self.page_content = ''
+
+    def _read_file(self):
+        with open(self.index_page_file_path, 'r') as f:
+            self.page_content = f.read()
+
+    def get_list_of_item_urls_from_index_page(self):
+        self._read_file()
+
+        regex = re.compile('https:\/\/\S*\/itm/\S{12}')
+
+        item_urls = set(regex.findall(self.page_content))
+        items_urls_cleaned = [i for i in item_urls if '12345' not in i]
+
+        print(f'Numbers of items found: {len(items_urls_cleaned)}')
+
+        return items_urls_cleaned
